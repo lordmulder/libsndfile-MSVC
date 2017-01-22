5d62fc7d4fe3f5c3d987c63c0d40d7d667899c6f
LoRd_MuldeR <mulder2@gmx.de>
2017-01-22 17:15:58 +0100

    Moved CSS file to "Prerequisites" directory.

--- mkdocs.cmd
+++ mkdocs.cmd
@@ -5,7 +5,7 @@ if not exist "%JAVA_HOME%\bin\java.exe" (
 	pause && goto:eof
 )
 
-"%~dp0\..\Prerequisites\Pandoc\pandoc.exe" --from markdown --to html5 --toc -N --standalone --self-contained -c "%~dp0\etc\css\github-pandoc.css" --output "%~dp0\README.html" "%~dp0\README.md"
+"%~dp0\..\Prerequisites\Pandoc\pandoc.exe" --from markdown --to html5 --toc -N --standalone --self-contained -c "%~dp0\..\Prerequisites\Pandoc\css\github-pandoc.css" --output "%~dp0\README.html" "%~dp0\README.md"
 
 "%JAVA_HOME%\bin\java.exe" -jar "%~dp0\..\Prerequisites\HTMLCompressor\bin\htmlcompressor-1.5.3.jar" --compress-css -o "%~dp0\README.min.html" "%~dp0\README.html"
 move /Y "%~dp0\README.min.html" "%~dp0\README.html"
